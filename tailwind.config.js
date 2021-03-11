8892479a8a9031f88ec370474cb5a0553b7e7ee8
boratechlife <74407169+boratechlife@users.noreply.github.com>
2021-03-11 13:21:57 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+
+            'display': ['Amatic'],
+            'sans': ['Andika'],
+        },
+        extend: {
+            theme: {
+
+                colors: {
+                    primary: "#ff9600"
+                }
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
